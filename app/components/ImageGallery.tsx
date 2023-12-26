8c3dc18e4bf46a0f45401a24cd9dd5b1909e389a
Malik-444 <talhahusnain475@gmail.com>
2023-12-26 23:55:40 +0500

    product slugPages ready

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { urlFor } from "../lib/sanity";
 
 interface iAppProps {
@@ -7,6 +8,11 @@ interface iAppProps {
 }
 
 export default function ImageGallery({ images }: iAppProps) {
+  const [bigImg, setBigImg] = useState(images[0]);
+  const handleClickImg = (image: any) => {
+    setBigImg(image);
+  };
+
   return (
     <div className="grid gap-4 lg:grid-cols-5">
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
@@ -17,10 +23,21 @@ export default function ImageGallery({ images }: iAppProps) {
               width={200}
               height={200}
               alt="photo"
-              className="h-full w-full object-cover object-center cursor"
+              className="h-full w-full object-cover object-center cursor-pointer"
+              onClick={() => handleClickImg(image)}
             />
           </div>
         ))}
+      </div>
+      <div className="relative overflow-hidden bg-gray-200 rounded-lg lg:col-span-4">
+        <Image
+          src={urlFor(bigImg).url()}
+          alt="big-img"
+          width={500}
+          height={500}
+          className="h-full w-full object-cover object-center"
+        />
+        <span className="absolute top-0 left-0 rounded-br-lg text-sm px-3 py-1.5 tracking-wider bg-red-500 uppercase text-white">sale</span>
       </div>
     </div>
   );
--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,8 +1,9 @@
-
 import ImageGallery from "@/app/components/ImageGallery";
 import { fullProduct } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
 import React from "react";
+import { FaStar } from "react-icons/fa";
+import { LiaShippingFastSolid } from "react-icons/lia";
 
 async function getData(slug: string) {
   const query = `*[_type=="product" && slug.current=="${slug}"][0]{
@@ -28,7 +29,52 @@ export default async function Productpage({
     <div className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="grid gap-8 md:grid-cols-2">
-         <ImageGallery images={data.images}/>
+          <ImageGallery images={data.images} />
+          <div className="md:py-8">
+            <div className="mb-2 md:mb-3">
+              <span className="mb-0.5 inline-block text-gray-500">
+                {data.categoryName}
+              </span>
+              <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
+                {data.name}
+              </h2>
+            </div>
+            <div className="mb-6 flex items-center gap-3 md:mb-10">
+              <button className=" px-2 py-1 flex items-center gap-x-2  rounded-full text-white bg-[#DC143C] hover:bg-[#DC143C]">
+                <span>4.3</span>
+                <FaStar size={16} />
+              </button>
+              <span className="text-sm text-gray-500 transition duration-100">
+                100+ Ratings
+              </span>
+            </div>
+            <div className="mb-6">
+              <div className="flex items-center gap-2">
+                <span className="text-xl font-bold md:text-2xl text-gray-800">
+                  ${data.price}
+                </span>
+                <span className="mb-0.5 text-red-500 line-through">
+                  ${data.price + 30}
+                </span>
+              </div>
+              <span className="text-sm text-gray-500">Incl shipping</span>
+            </div>
+            <div className="mb-6 flex items-center gap-2 text-gray-500">
+              <LiaShippingFastSolid size={20} />
+              <span className="text-sm">2-4 days shipping</span>
+            </div>
+            <div className="mb-6 flex gap-x-2 ">
+              <button className="btn bg-[#DC143C] text-white hover:bg-white hover:text-[#DC143C] transition">
+                Add to Cart
+              </button>
+              <button className="btn bg-[#DC143C] text-white hover:bg-white hover:text-[#DC143C] transition">
+                Checkout Now
+              </button>
+            </div>
+            <p className="text-base text-gray-500 tracking-wide mt-12">
+              {data.description}
+            </p>
+          </div>
         </div>
       </div>
     </div>
